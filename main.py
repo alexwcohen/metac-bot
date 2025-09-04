31ec6cd6caeb821931883763c82f7305c2acb902
alexwcohen <80180528+alexwcohen@users.noreply.github.com>
2025-09-04 14:46:58 -0400

    Fixed bug

--- main.py
+++ main.py
@@ -592,17 +592,17 @@ if __name__ == "__main__":
         publish_reports_to_metaculus=True, ## TOGGLE
         folder_to_save_reports_to=None,
         skip_previously_forecasted_questions=True,
-        llms={  # choose your model names or GeneralLlm llms here, otherwise defaults will be chosen for you
-             "default": GeneralLlm(
-                 model="openrouter/openai/gpt-5", # "anthropic/claude-3-5-sonnet-20241022", etc (see docs for litellm)
-                 temperature=0.3,
-                 timeout=40,
-                 allowed_tries=2,
-             ),
+        # llms={  # choose your model names or GeneralLlm llms here, otherwise defaults will be chosen for you
+        #     "default": GeneralLlm(
+        #         model="openrouter/openai/gpt-4o", # "anthropic/claude-3-5-sonnet-20241022", etc (see docs for litellm)
+        #         temperature=0.3,
+        #         timeout=40,
+        #         allowed_tries=2,
+        #     ),
         #     "summarizer": "openai/gpt-4o-mini",
-             "researcher": "asknews/deep-research/low",
+        #     "researcher": "asknews/deep-research/low",
         #     "parser": "openai/gpt-4o-mini",
-        },
+        # },
     )
 
     if run_mode == "tournament":
