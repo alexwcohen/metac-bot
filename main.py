31c68022010a0c9afd125a9dbcdc20c59f2eb98f
alexwcohen <80180528+alexwcohen@users.noreply.github.com>
2025-09-04 17:13:40 -0400

    Fixed small error

--- main.py
+++ main.py
@@ -595,9 +595,9 @@ if __name__ == "__main__":
                  timeout=40,
                  allowed_tries=2,
              ),
-             "summarizer": "openai/gpt-4o-mini",
-             "researcher": "asknews/deep-research/low",
-             "parser": "openai/gpt-4o-mini",
+        #     "summarizer": "openai/gpt-4o-mini",
+        #     "researcher": "asknews/deep-research/low",
+        #     "parser": "openai/gpt-4o-mini",
         },
     )
 
